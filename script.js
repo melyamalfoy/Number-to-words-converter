0fb9e114891e037379a9bddb9337349d8271c319
melyamalfoy <melanieegas@gmail.com>
2023-05-05 11:21:09 +0200

    implemented 10.000 and added some commenhts

--- script.js
+++ script.js
@@ -20,8 +20,9 @@ function convertNumber() {
 function numberToWord(number) {
     // Deze arrays definiëren de woorden voor eenheden, tientallen en getallen van 11 tot en met 19
     const eenheden = ["nul", "een", "twee", "drie", "vier", "vijf", "zes", "zeven", "acht", "negen"];
+    //aanpassen met lege stirng beginnen
+    const elfTotNegentien = ["", "elf", "twaalf", "dertien", "veertien", "vijftien", "zestien", "zeventien", "achttien", "negentien"];
     const tientallen = ["", "tien", "twintig", "dertig", "veertig", "vijftig", "zestig", "zeventig", "tachtig", "negentig"];
-    const elfTotNegentien = ["elf", "twaalf", "dertien", "veertien", "vijftien", "zestien", "zeventien", "achttien", "negentien"];
   
     // Deze functie zet een getal om naar een woord
     function convertToWords(number) {
@@ -30,18 +31,9 @@ function numberToWord(number) {
         return eenheden[number];
       }
       // Als het getal 10 t/m 19 is geef dan het getal van de overeenkomstige index uit de elfTotNegentien-array terug
-      // Deze is apart omdat de getallen van 11 t/m 19 niet volgens een vast patroon gaan
-
-    //   if (number === 10 || (number > 10 && number < 20)) { // als number 10 is of tussen 10 en 20
-    //     if (number === 10) {
-    //       return tientallen[number / 10];
-    //     } else {
-    //       return elfTotNegentien[number - 11];
-    //     }
-    //   }  vb van mijn originele code
 
     if (number >= 11 && number <= 19) {
-        return elfTotNegentien[number - 11];
+        return elfTotNegentien[number - 10];
       } else if (number === 10) {
         return tientallen[1];
       } 
@@ -55,7 +47,7 @@ function numberToWord(number) {
       //tientallen 3 = dertig / 10 = 3 (index 3 in tientallen array)
       // dertig + zeven = zevenendertig
       if (number >= 20 && number < 100) {
-        return (number % 10 > 0 ? eenheden[number % 10] + "en" : "") + tientallen[Math.floor(number / 10)]; 
+        return (number % 10 > 0 ? eenheden[number % 10] + "en" : "") + tientallen[Math.floor(number / 10) + ""]; 
       }
 
       // Als het getal tussen 100 en 1000 ligt, retourneer dan het woord voor het honderdtal gevolgd door het woord voor het resterende getal.
@@ -63,11 +55,16 @@ function numberToWord(number) {
         return (Math.floor(number / 100) === 1 ? "" : eenheden[Math.floor(number / 100)]) + "honderd" + (number % 100 > 0 ? "en" + convertToWords(number % 100) : "");
       }
       
+      // getal tussen 1000 en 10.000
       if (number >= 1000 && number < 10000) {
         return (Math.floor(number / 1000) === 1 ? "" : eenheden[Math.floor(number / 1000)]) + "duizend" + (number % 1000 > 0 ? convertToWords(number % 1000) : "");
       }
       
+    // getal tussen 10.000 en 100.000
       if (number >= 10000 && number < 100000) {
+        if(number === 10000){
+          return "tienduizend";
+        }
         return (number % 10000 > 0 ? convertToWords(Math.floor(number / 1000)) : "") + "duizend" + (number % 1000 > 0 ? convertToWords(number % 1000) : "");
       }
       
@@ -78,7 +75,8 @@ function numberToWord(number) {
     // Geeft het resultaat van de functie terug
     return convertToWords(number);
   }
-  console.log(numberToWord(0)); 
+  console.log(numberToWord(10000)); 
+  console.log(numberToWord(12345));
   console.log(numberToWord(1)); 
   console.log(numberToWord(10)); 
   console.log(numberToWord(11)); 
